9e04e05a1fa51b7a39284b52c46edc7bdebd6ced
SRaiz <sidharthraizada07@gmail.com>
2020-02-27 23:34:14 +0000

    Updated Settings.py

--- Krishi_Karma/settings.py
+++ Krishi_Karma/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'Krishi_Karma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'krishi-karma',
+        'USER': 'postgres',
+        'PASSWORD': 'tweety_336600',
+        'HOST': 'localhost'
     }
 }
 
