58c3135c01ac401e543455733923170ffa722b84
Sidharth Raizada <31130037+SRaiz@users.noreply.github.com>
2020-06-25 22:01:29 +0100

    Update wsgi.py

--- Krishi_Karma/wsgi.py
+++ Krishi_Karma/wsgi.py
@@ -7,17 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# ML registry
-import inspect
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-from apps.ml.cropsyield_classifier.random_forest import RandomForestClassifier
-from apps.ml.registry import MLRegistry
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Krishi_Karma.settings')
 application = get_wsgi_application()
+
+# ML registry
+import inspect
+from apps.ml.registry import MLRegistry
+from apps.ml.cropsyield_classifier.random_forest import RandomForestClassifier
 
 
 
