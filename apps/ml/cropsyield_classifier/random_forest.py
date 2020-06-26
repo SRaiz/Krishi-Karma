b5142c84dc93621aca7603217cbd7716f3e54d8d
SRaiz <sidharthraizada07@gmail.com>
2020-06-26 11:31:30 +0100

    Updated UI

--- apps/ml/cropsyield_classifier/random_forest.py
+++ apps/ml/cropsyield_classifier/random_forest.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import joblib
 import pandas as pd
 
@@ -11,16 +14,33 @@ class RandomForestClassifier:
 
     
     def preprocessing(self, input_data):
-        # JSON to pandas DataFrame
-        input_data = pd.DataFrame(input_data, index=[0])
-        # fill missing values
-        input_data.fillna(self.values_fill_missing)
-        # convert categoricals
-        for column in [ "state_name", "district_name", "season", "crop"]:
-            categorical_convert = self.encoders[column]
-            input_data[column] = categorical_convert.transform(input_data[column])
+        try:
+            # JSON to pandas DataFrame
+            input_data = pd.DataFrame(input_data, index=[0])
+            # fill missing values
+            input_data.fillna(self.values_fill_missing)
+            # convert categoricals
+            for column in [ "state_name", "district_name", "season", "crop"]:
+                categorical_convert = self.encoders[column]
+                input_data[column] = categorical_convert.transform(input_data[column])
 
-        return input_data
+            return input_data
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
+            print()
+            print(exc_type)
+            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
+            print()
+            print(fname)
+            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
+            print()
+            print(exc_tb.tb_lineno)
+            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
+            print()
+            print(str(e))
+            return ''
 
     
     def predict(self, input_data):
@@ -39,14 +59,26 @@ class RandomForestClassifier:
 
 
     def compute_prediction(self, input_data):
-        print(input_data)
         try:
             input_data = self.preprocessing(input_data)
             prediction = self.predict(input_data)[0]  # only one sample
             prediction = self.postprocessing(prediction)
         
         except Exception as e:
-            print('WWWWWWWWW:: '+str(e))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print('+++++++++++++++++++++++++++++++')
+            print()
+            print(exc_type)
+            print('+++++++++++++++++++++++++++++++')
+            print()
+            print(fname)
+            print('+++++++++++++++++++++++++++++++')
+            print()
+            print(exc_tb.tb_lineno)
+            print('+++++++++++++++++++++++++++++++')
+            print()
+            print(str(e))
             return {
                 "status": "Error", 
                 "message": str(e)
