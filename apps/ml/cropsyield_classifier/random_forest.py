dc567dc44aa89f625438731a892a64e3b4025e9c
SRaiz <sidharthraizada07@gmail.com>
2020-06-26 18:58:17 +0100

    Updated rf

--- apps/ml/cropsyield_classifier/random_forest.py
+++ apps/ml/cropsyield_classifier/random_forest.py
@@ -14,33 +14,16 @@ class RandomForestClassifier:
 
     
     def preprocessing(self, input_data):
-        try:
-            # JSON to pandas DataFrame
-            input_data = pd.DataFrame(input_data, index=[0])
-            # fill missing values
-            input_data.fillna(self.values_fill_missing)
-            # convert categoricals
-            for column in [ "state_name", "district_name", "season", "crop"]:
-                categorical_convert = self.encoders[column]
-                input_data[column] = categorical_convert.transform(input_data[column])
+        # JSON to pandas DataFrame
+        input_data = pd.DataFrame(input_data, index=[0])
+        # fill missing values
+        input_data.fillna(self.values_fill_missing)
+        # convert categoricals
+        for column in [ "state_name", "district_name", "season", "crop"]:
+            categorical_convert = self.encoders[column]
+            input_data[column] = categorical_convert.transform(input_data[column])
 
-            return input_data
-        except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
-            print()
-            print(exc_type)
-            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
-            print()
-            print(fname)
-            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
-            print()
-            print(exc_tb.tb_lineno)
-            print('RRRRRRR$$$$$$$$$$$$$$$$$$')
-            print()
-            print(str(e))
-            return ''
+        return input_data
 
     
     def predict(self, input_data):
@@ -67,14 +50,6 @@ class RandomForestClassifier:
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print('+++++++++++++++++++++++++++++++')
-            print()
-            print(exc_type)
-            print('+++++++++++++++++++++++++++++++')
-            print()
-            print(fname)
-            print('+++++++++++++++++++++++++++++++')
-            print()
             print(exc_tb.tb_lineno)
             print('+++++++++++++++++++++++++++++++')
             print()
